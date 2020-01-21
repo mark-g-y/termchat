cb390287ad02b8b962e733bd1e57a5a86cb62ecb
Shawn Ren <sren@linkedin.com>
2020-01-20 18:02:59 -0800

    Add further UI for output and inputting text

--- client/app.py
+++ client/app.py
@@ -1,49 +1,110 @@
 import curses
 from curses import wrapper
 
-# class AppState(Enum):
-#   WELCOME = 1
-#   CHAT = 2
-
 class App:
   def __init__(self, screen):
+    # Curses Library setup
     self.screen = screen
+    self.window = self.screen.subwin(5, 0)
+    self.window.scrollok(True)
 
-  def print_headline(self, message):
+    # Variables used in app
+    self.banner = None
+    self.welcome_banner = "**** Welcome to TermChat! The illest chat network on the web ****"
+    self.chatrooms = None
+    self.selected_chatroom = None
+    self.selected_chatroom_messages = ["guest1: yo", "guest2: yo", "guest1: yo", "guest2: yo", "guest3: YOOOOOOOO"]
+
+    # Initialize app
+    self.banner = self.welcome_banner
+    self.render_banner()
+
+    self.chatrooms = self.get_chatrooms()
+    self.render_waiting_lobby()
+
+    self.prompt_lobby_input()
+
+  def render_banner(self):
+    title = self.banner
+    title_len = len(title)
+
+    # Calculate stuff for rendering title
     num_rows, num_cols = self.screen.getmaxyx()
     y_position = 0
-
-    half_length_of_message = int(len(message) / 2)
+    half_length_of_title = int(len(title) / 2)
     middle_column = int(num_cols / 2)
-    x_position = middle_column - half_length_of_message
+    x_position = middle_column - half_length_of_title
 
-    message_margin = len(message) * "*"
+    # Calculate stuff for rendering subtitle
+    subtitle = self.render_banner_subtitle_helper(title_len)
 
-    self.screen.addstr(y_position + 0, x_position, message_margin)
-    self.screen.addstr(y_position + 1, x_position, message)
-    self.screen.addstr(y_position + 2, x_position, message_margin)
-    self.screen.addstr("\n\n")
+    # Calculate stuff for rendering margins
+    title_margin = title_len * "*"
+
+    self.screen.addstr(y_position + 0, x_position, title_margin)
+    self.screen.addstr(y_position + 1, x_position, title)
+    self.screen.addstr(y_position + 2, x_position, subtitle)
+    self.screen.addstr(y_position + 3, x_position, title_margin)
     self.screen.refresh()
 
-  def print_chatrooms_list(self, chatrooms):
-    self.screen.addstr("Chatrooms available:\n")
+  def render_banner_subtitle_helper(self, message_len):
+    if self.selected_chatroom == None:
+      chatroom = "N/A"
+    else:
+      chatroom = self.selected_chatroom
 
-    for chatroom in chatrooms:
-      self.screen.addstr("- " + chatroom + "\n")
+    text_to_render_inner = "      Current chatroom: " + chatroom + "      "
+    text_to_render_margin_len = (message_len - len(text_to_render_inner)) / 2
+    text_to_render_margin_additional_len = (message_len - len(text_to_render_inner)) % 2
+    text_to_render_margin_left = text_to_render_margin_len * "*"
+    text_to_render_margin_right = (text_to_render_margin_len + text_to_render_margin_additional_len) * "*"
+    text_to_render = text_to_render_margin_left + text_to_render_inner + text_to_render_margin_right
+    return text_to_render
 
-    self.screen.addstr("\n")
-    self.screen.refresh()
+  def render_waiting_lobby(self):
+    self.window.clear()
 
-  def input_subscribe_chatroom(self):
-    self.screen.addstr("Please enter the name of the chatroom you'd like to join:\n")
-    self.screen.refresh()
+    self.window.addstr("Chatrooms available:\n")
+    for chatroom in self.chatrooms:
+      self.window.addstr("- " + chatroom + "\n")
+    self.window.addstr("\n")
+
+    self.window.refresh()
+
+  def render_chatroom(self):
+    self.window.clear()
+
+    self.window.addstr("User has entered \"" + self.selected_chatroom + "\"\n")
+    for message in self.selected_chatroom_messages:
+      self.window.addstr(message + "\n")
+
+    self.window.refresh()
+
+  def get_chatrooms(self):
+    return ["the gangsta club", "flat earthers", "dinasour gang"]
+
+  def prompt_lobby_input(self):
+    self.window.addstr("Please enter the name of the chatroom you'd like to join:\n")
+    self.window.refresh()
     
-    self.user_input = self.screen.getstr()
-    self.screen.addstr("You selected: \"" + self.user_input + "\"\n")
-    self.screen.refresh()
+    user_input = self.window.getstr()
+    if user_input in set(self.chatrooms):
+      self.selected_chatroom = user_input
+      self.render_banner()
+      self.render_chatroom()
+    else:
+      self.window.addstr(user_input + " is not an existing chatroom. Creating...")
+      self.window.refresh()
+      curses.napms(1000)
+      self.selected_chatroom = user_input
+      self.render_banner()
+      self.render_chatroom()
 
-  def batch_get_chatrooms(self):
-    return ["gangsta club", "flat earthers", "dinasour gang"]
+    self.window.refresh()
+
+  def print_chatroom(self):
+    self.window.scroll(1)
+    self.window.refresh()
 
 
 def main(stdscr):
@@ -51,13 +112,6 @@ def main(stdscr):
   curses.echo()
 
   app = App(screen)
-
-  app.print_headline("**** Welcome to TermChat! The illest chat network on the web ****")
-
-  chatrooms = app.batch_get_chatrooms()
-  app.print_chatrooms_list(chatrooms)
-
-  app.input_subscribe_chatroom()
 
   stdscr.getch()
   curses.endwin()
